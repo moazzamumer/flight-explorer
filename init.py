ca2ea821a74934dd6740cbcad968126bfb4154df
Moazzam Umer <moazzamumar22@gmail.com>
2024-05-09 01:33:26 +0500

    refactorized

--- init.py
+++ init.py
@@ -9,16 +9,17 @@ async def flights_information_endpoint(user_input: base_models.UserTextInput):
 
     ai_model = gemini_ai.GenAI(constants.GOOGLE_API_KEY)
     scrapper = scrap.FlightScraper()
+
     # Parse given text input into JSON using AI model
     parsed_info = ai_model.get_parsed_input(user_input.text)
-    print(parsed_info)
+    # print(parsed_info)
 
     # Scrap flights information from internet
     flights = scrapper.get_flight_info(json_data=parsed_info)
-    print(flights)
+    # print(flights)
     if flights:
         response = ai_model.parse_flight_info(flights)
-        print(response)
+        # print(response)
         return response
     else:
         raise HTTPException(status_code=404, detail="No flights found")
